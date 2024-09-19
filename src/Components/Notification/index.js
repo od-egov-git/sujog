37e8d736dd455eb60723b594055f984e8c199201
dsatapathy <digdarshan.satapathy@pwc.com>
2024-09-19 21:18:03 +0530

    Added Notification on Sujog Portal

--- src/Components/Notification/index.js
+++ src/Components/Notification/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css'; // Import your CSS file for styles
 
 const Notification = () => {
-  const message = "The SUJOG site is currently undergoing maintenance and is unavailable between 07 March at 10 PM to 10 March 2024 till 12:PM IST.";
+  const message = "SUJOG Portal will be undergoing scheduled maintenance from 7:00 PM on 20th September 2024 to 6:00 AM on 23rd September 2024. During this period, the portal will be temporarily unavailable.";
 
   return (
     <div className="notification-bar">
