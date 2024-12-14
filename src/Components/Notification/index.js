ad3b90d661b58bed817a219ee373a07b5aeee6a7
dsatapathy <digdarshan.satapathy@pwc.com>
2024-12-14 10:51:16 +0530

    Notification added

--- src/Components/Notification/index.js
+++ src/Components/Notification/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css'; // Import your CSS file for styles
 
 const Notification = () => {
-  const message = "SUJOG Portal will be undergoing scheduled maintenance from 7:00 PM on 20th September 2024 to 6:00 AM on 23rd September 2024. During this period, the portal will be temporarily unavailable.";
+  const message = "SUJOG Portal will be undergoing server maintenance from 7:00 PM on 14th December 2024 to 7:00 PM on 15th December 2024. During this period, the portal will be temporarily unavailable.";
 
   return (
     <div className="notification-bar">
