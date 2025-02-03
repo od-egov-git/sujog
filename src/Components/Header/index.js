5582d796cf321aa27050d57f80f3ceb2c3d9b94f
dsatapathy <digdarshan.satapathy@pwc.com>
2025-02-03 16:15:34 +0530

    Fix email harvesting for header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -7,7 +7,7 @@ class Header extends Component {
             <div className="container clearfix">
                 <div className="contact-info float-left">
                     <span><i className="icofont-phone"></i> 1800 121 6833</span>
-                    <span><i className="icofont-envelope"></i><a href="mailto:contact@example.com"> helpdesk.sujog@odisha.gov.in</a></span>
+                    <span><i className="icofont-envelope"></i><img src="/Deshboard/images/contact_email1.PNG" alt="Contact Email" /></span>
 
                 </div>
             </div>
