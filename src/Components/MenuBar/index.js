25db29a206b66adc107df607a8b3b51d9c9a313e
Pritam K Sahu <pritam.k.sahu.tpr@pwc.com>
2025-10-16 15:42:07 +0530

    Urban Conclave Registration link new page with pop up  16/10/2025-01

--- src/Components/MenuBar/index.js
+++ src/Components/MenuBar/index.js
@@ -66,13 +66,7 @@ function Menubar() {
                             <li>
                                 <Link to="privacy-policy">{translations.navigationPrivacyPolicy}</Link>
                             </li>
-                            <li>
-                                <a className="dropdown-item d-lg-block" style={{ color: "#000" }} href={`${baseUrl}/citizen/withoutAuth/egov-usm/search`}>
-                                    {translations.navigationRegistration}
-                                </a>
-
-                                {/* <Link to="Registration">{translations.navigationRegistration}</Link> */}
-                            </li>
+                            
                         </ul>
                     </li>
                     <li>
@@ -81,6 +75,11 @@ function Menubar() {
                     <li>
                         <Link to="/contactus">{translations.navigationHelpdesk}</Link>
                     </li>
+                    <li>
+                                
+
+                                <Link to="Registration">{translations.navigationRegistration}</Link>
+                            </li>
                     <li className="login" style={{ marginLeft: "20px" }}>
                         <div className="dropdown">
                             <div className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
