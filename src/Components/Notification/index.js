804dc38c7ae7b3fe55d6f21f152f32cd2aa41b20
soumyadip-bairagi-pwc <soumyadip.bairagi@pwc.com>
2025-10-22 19:01:32 +0530

    Banner Message

--- src/Components/Notification/index.js
+++ src/Components/Notification/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './index.css'; // Import your CSS file for styles
 
 const Notification = () => {
-  const message = "SUJOG Services will be temporarily unavailable from 3:00 PM onwards on 23rd October 2025, for SDC storage maintenance. Services will resume once the activity is completed.";
+  const message = "SUJOG Services will be temporarily unavailable from 23rd October 2025 3:00 PM onwards, for SDC storage maintenance. Services will resume once the activity is completed.";
 
   return (
     <div className="notification-bar">
